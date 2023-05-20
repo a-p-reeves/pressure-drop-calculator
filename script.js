7026729e5067643441215fc18258962facdb3ae5
a-p-reeves <133796805+a-p-reeves@users.noreply.github.com>
2023-05-20 18:23:34 +0100

    inclusion of Reynolds number

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function ergun() {
+    
+    // inputs
+
     let sphericity = document.getElementById('sphericity').value        // sphericity
     let dp = document.getElementById('dp').value/1000                   // particle diameter in m
     let e = document.getElementById('e').value                          // bed porosity
@@ -9,15 +12,23 @@ function ergun() {
     let dc = document.getElementById('dc').value                        // column diameter
     let L = document.getElementById('L').value                          // column height
     
-    let flowrate = (m/rho_g);
-    let V = flowrate/(3.1415926535*Math.pow(dc/2,2));                // gas velocity from geometry and density
+    // calculations
 
-    let deltaP = (150*mu_g*Math.pow((1-e),2)*V)/(Math.pow(sphericity,2)*Math.pow(dp,2)*Math.pow(e,3))+((1.75*rho_g*(1-e)*V*Math.abs(V))/(sphericity*dp*Math.pow(e,3)));
-    let deltaPtotal = deltaP*L;
+    let flowrate = (m/rho_g);
+    let V = flowrate/(3.1415926535*Math.pow(dc/2,2));                   // gas velocity from geometry and density
+
+    let deltaP = (150*mu_g*Math.pow((1-e),2)*V)/(Math.pow(sphericity,2)*Math.pow(dp,2)*Math.pow(e,3))+((1.75*rho_g*(1-e)*V*Math.abs(V))/(sphericity*dp*Math.pow(e,3))); // Ergun Equation
+    let deltaPtotal = deltaP*L;                                         // Ergun equation extended for entire bed length L
+
+    let Re = (rho_g*V*dp)/(mu_g*(1-e));                                 // Reynold's number
+
+    // outputs
 
     document.getElementById('vflow-output').innerHTML = (flowrate*3600).toFixed(2)+' m<sup>3</sup>/h';
 
-    document.getElementById('sflow-output').innerHTML = V.toFixed(2)+' m/s';
+    document.getElementById('sflow-output').innerHTML = V.toFixed(3)+' m/s';
+
+    document.getElementById('reynolds').innerHTML = Re.toFixed(3);
 
     document.getElementById('pdrop-output').innerHTML = (deltaP/100000).toFixed(5)+' bar';
 
